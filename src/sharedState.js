491b16f89041b359e9547f06204dfe75f9f46a67
Eddie Atkinson <45678264+eddie-atkinson@users.noreply.github.com>
2024-09-02 14:20:32 +0800

    gooo

--- src/sharedState.js
+++
@@ -1,5 +0,0 @@
-import { SharedPersistedState } from './hooks/useSharedState';
-
-export const parcels = new SharedPersistedState('parcels', []);
-export const adjustments = new SharedPersistedState('adjustments', []);
-export const sales = new SharedPersistedState('sales', []);
